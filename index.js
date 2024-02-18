a82cc0b5eabc17416bb02ec10004e7540d82267d
fauzihiz <37166084+fauzihiz@users.noreply.github.com>
2024-02-18 11:51:29 +0700

    revamp menu

--- index.js
+++ index.js
@@ -12,22 +12,3 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 
-// Add click event listener to each image
-var images = document.querySelectorAll('.swiper-slide img');
-images.forEach(function (image) {
-  image.addEventListener('click', function () {
-    // Open image in a modal
-    var modal = document.createElement('div');
-    modal.classList.add('modal');
-    var modalImg = document.createElement('img');
-    modalImg.src = this.src;
-    modal.appendChild(modalImg);
-    document.body.appendChild(modal);
-
-    // Close modal when clicked outside of the image
-    modal.addEventListener('click', function () {
-      document.body.removeChild(modal);
-    });
-  });
-});
-
